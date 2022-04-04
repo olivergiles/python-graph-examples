b15b4c66e4f763942e77ad913878935750fb4be9
Oliver Giles <ogiles@email.com>
2022-04-04 16:01:43 +0000

    added labled graph

--- matplotlib/simple/labeled.py
+++ matplotlib/simple/labeled.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 
 def main():
     plt.plot([1, 2, 3, 4])
+    plt.ylabel("some numbers")
+    plt.xlabel("numbers in order")
+    plt.title("Random graph")
     plt.savefig("line.png")
     plt.show()
 
